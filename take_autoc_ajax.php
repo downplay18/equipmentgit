15d4759e2c9b2cf03c6eead25fbbf6d309beba99
downplay18 <downplay18@Flatt-PC>
2016-10-06 14:23:22 +0700

    แก้ แสดงเฉพาะของที่ตัวเองเป็น owner

--- take_autoc_ajax.php
+++ take_autoc_ajax.php
@@ -1,10 +1,12 @@
 <?php
+session_start();
 /*สำหรับ autocomplete ใช้ใน 'add' และ 'take'*/
 require_once 'connection.php';
 if ($_POST['type'] == 'item_table') {
     $row_num = $_POST['row_num'];
     $name = $_POST['name_startsWith'];
-    $query = "SELECT `detail`,`suffix` FROM `item` WHERE `detail` LIKE '%" . $name . "%'";
+    $query = "SELECT `detail`,`suffix`,`owner` FROM `item` WHERE `detail` LIKE '%" . $name . "%'"
+            . " AND `owner` LIKE '".$_SESSION['name']."'";
     $result = mysqli_query($connection, $query);
     $data = array();
     while ($row = mysqli_fetch_assoc($result)) {
