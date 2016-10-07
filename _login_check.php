00157133e998f143482b506412ab1c745253be2d
downplay18 <downplay18@Flatt-PC>
2016-10-07 15:34:04 +0700

    add SESSION myboss to KEYuser

--- _login_check.php
+++ _login_check.php
@@ -24,11 +24,11 @@ if ($_SESSION['user_id'] != "") {
 /* รับ $_POST มาจาก navbar_unauthen.php */
 /* $_POST ของ Username = login_cid */
 /* $_POST ของ Password = login_pwd */
-    require("connection.php");
-    $strSQL = "SELECT `user_id`,`status`,`name`,`division` FROM user WHERE `user_id` = '" . mysql_real_escape_string($_POST["login_cid"]) . "'
+require("connection.php");
+$strSQL = "SELECT `user_id`,`status`,`name`,`division`,`myboss` FROM user WHERE `user_id` = '" . mysql_real_escape_string($_POST["login_cid"]) . "'
 	AND `password` = '" . mysql_real_escape_string($_POST["login_pwd"]) . "'"; /* ไม่ SELECT `Password` เพราะไม่ได้แสดงค่ามัน */
-    $loginQuery = mysqli_query($connection, $strSQL) or die("_login_check.php คิวรี่ล้มเหลว!");
-    $loginResult = mysqli_fetch_assoc($loginQuery); /* ได้ loginResult เป็นผลลัพธ์ของการ query */
+$loginQuery = mysqli_query($connection, $strSQL) or die("_login_check.php คิวรี่ล้มเหลว!");
+$loginResult = mysqli_fetch_assoc($loginQuery); /* ได้ loginResult เป็นผลลัพธ์ของการ query */
 /* =====/_login_connection===== */
 
 /* เช็คว่า Username กับ Password ที่ป้อนเข้ามา มีใน db หรือไม่ */
@@ -39,13 +39,16 @@ if (!$loginResult) {
     $_SESSION["status"] = $loginResult["status"];
     $_SESSION["name"] = $loginResult["name"];
     $_SESSION["division"] = $loginResult['division'];
-    
+    if ($loginResult['status'] == "KEY") {
+        $_SESSION['myboss'] = $loginResult['myboss'];
+    }
+
     //unset($loginResult);
     session_write_close();
 
     if ($loginResult["status"] == "BOSS") {
         header("location:_login_boss.php");
-    } elseif($loginResult["status"] == "USER") {
+    } elseif ($loginResult["status"] == "USER") {
         header("location:_login_user.php");
     } else {
         header("location:index.php");
