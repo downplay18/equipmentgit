b4271faa2d914d6b8176a0e16bb353680e6c55ef
downplay18 <downplay18@Flatt-PC>
2016-10-13 10:01:52 +0700

    เพิ่มเงื่อนไข `owner` ในการค้นหา

--- add_autoc_ajax.php
+++ add_autoc_ajax.php
@@ -1,10 +1,11 @@
 <?php
 /*สำหรับ autocomplete ใช้ใน 'add' และ 'take'*/
+session_start();
 require_once 'connection.php';
 if ($_POST['type'] == 'item_table') {
     $row_num = $_POST['row_num'];
     $name = $_POST['name_startsWith'];
-    $query = "SELECT `detail`,`suffix` FROM `item` WHERE `detail` LIKE '%" . $name . "%'";
+    $query = "SELECT `detail`,`suffix` FROM `item` WHERE `detail` LIKE '%" . $name . "%' AND `owner` LIKE '". $_POST['division'] ."'";
     $result = mysqli_query($connection, $query);
     $data = array();
     while ($row = mysqli_fetch_assoc($result)) {
