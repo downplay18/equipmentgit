f6c3c4c77e1feef14f9ea99bd7e9232acfdc0e56
downplay18 <downplay18@Flatt-PC>
2016-10-18 22:34:20 +0700

    include datatables

--- main_head.php
+++ main_head.php
@@ -8,9 +8,11 @@
 <!-- Bootstrap Core CSS -->
 <link href="css/bootstrap.min.css" type="text/css" rel="stylesheet" >
 
-<!-- Custom CSS
-<link href="css/modern-business.css" rel="stylesheet">
--->
+<!-- ทำให้tableใช้ live search ได้ -->
+<link href="css/datatables/jquery.dataTables-1.10.12.min.css" rel="stylesheet" type="text/css"/>
+
+<!-- ทำให้tableใช้ export/print ได้ -->
+<link href="css/datatables/buttons.dataTables-1.2.2.min.css" rel="stylesheet" type="text/css"/>
 
 <link href="favicon.ico" rel="icon">
 <link href="css/style.css" rel="stylesheet" type="text/css">     
