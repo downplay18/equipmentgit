d6a844380927a9aa829df2883f38eca65eb92f1c
downplay18 <downplay18@Flatt-PC>
2016-10-20 10:52:07 +0700

    include library สำหรับ datatable

--- main_head.php
+++ main_head.php
@@ -7,12 +7,20 @@
 
 <!-- Bootstrap Core CSS -->
 <link href="css/bootstrap.min.css" type="text/css" rel="stylesheet" >
+<link href="css/bootstrap-3.3.7/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+<link href="css/datatables/extensions/Buttons/css/buttons.bootstrap.min.css" rel="stylesheet" type="text/css"/>
 
-<!-- ทำให้tableใช้ live search ได้ -->
-<link href="css/datatables/jquery.dataTables-1.10.12.min.css" rel="stylesheet" type="text/css"/>
+<!-- สำหรับautocomplete ของ add.php -->
+<link href="css/jquery-ui.min.css" rel="stylesheet" type="text/css"/>
 
 <!-- ทำให้tableใช้ export/print ได้ -->
-<link href="css/datatables/buttons.dataTables-1.2.2.min.css" rel="stylesheet" type="text/css"/>
+<link href="css/datatables/extensions/Buttons/css/buttons.dataTables.min.css" rel="stylesheet" type="text/css"/>
+
+<!-- ทำให้tableใช้ live search ได้ -->
+<link href="css/datatables/media/css/jquery.dataTables.min.css" rel="stylesheet" type="text/css"/>
+
+
+
 
 <link href="favicon.ico" rel="icon">
 <link href="css/style.css" rel="stylesheet" type="text/css">     
--- main_script.php
+++ main_script.php
@@ -3,5 +3,18 @@
 <script src="js/jquery-1.12.2.min.js" type="text/javascript"></script>
 <script src="js/bootstrap.min.js"></script>
 
+<!-- สำหรับautocomplete ของ add.php -->
+<script src="js/jquery-ui.min.js" type="text/javascript"></script>
 
+<!-- <script src="//code.jquery.com/jquery-1.12.3.js" type="text/javascript"></script> -->
+<script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js" type="text/javascript"></script>
+<script src="https://cdn.datatables.net/buttons/1.2.2/js/dataTables.buttons.min.js" type="text/javascript"></script>
+<!-- <script src="//cdn.datatables.net/buttons/1.2.2/js/buttons.flash.min.js" type="text/javascript"></script> -->
+<script src="//cdnjs.cloudflare.com/ajax/libs/jszip/2.5.0/jszip.min.js" type="text/javascript"></script>
+<!-- <script src="//cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/pdfmake.min.js" type="text/javascript"></script> -->
+<!-- <script src="//cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/vfs_fonts.js" type="text/javascript"></script> -->
+<script src="//cdn.datatables.net/buttons/1.2.2/js/buttons.html5.min.js" type="text/javascript"></script>
+<script src="//cdn.datatables.net/buttons/1.2.2/js/buttons.print.min.js" type="text/javascript"></script>
 
+<script src="js/datatables/extensions/Buttons/js/buttons.bootstrap.min.js" type="text/javascript"></script>
+<script src="js/datatables/extensions/Buttons/js/buttons.colVis.min.js" type="text/javascript"></script>
