901379fcf3ea9aa8d25410ae989c67eec3ac9c92
downplay18 <downplay18@Flatt-PC>
2016-10-24 18:53:31 +0700

    แก้ GET แยกของเคส site

--- show.php
+++ show.php
@@ -21,55 +21,70 @@ $divSiteQS = "";
 $tableHeader = "";
 $tableData = "";
 $qryMsg = "";
+$case = 0;
 
 if (isset($_POST['submitBtn'])) {
     $_SESSION['lastDiv'] = $_POST['divName'];
     $_SESSION['lastSite'] = $_POST['siteName'];
     do {
-        //ไม่ได้เลือก
+//ไม่ได้เลือก
         if ($_POST['divName'] == "-- แยกตามกลุ่มงาน --" && $_POST['siteName'] == "-- แยกตามสถานที่ใช้งาน --") {
-            //echo '1-- แยกตามกลุ่มงาน -- -- แยกตามสถานที่ใช้งาน --';
+//echo '1-- แยกตามกลุ่มงาน -- -- แยกตามสถานที่ใช้งาน --';
             $qryMsg = "(ไม่มีค่าถูกเลือก)";
+            $case = -1;
             break;
         }
-        //เลือกทั้งกลุ่มงาน+สถานที่ใช้งาน
+//เลือกทั้งกลุ่มงาน+สถานที่ใช้งาน
         if ($_POST['divName'] != "-- แยกตามกลุ่มงาน --" && $_POST['siteName'] != "-- แยกตามสถานที่ใช้งาน --") {
-            //echo '99 เลือกทั้งกลุ่มงานและสถานที่ใช้งาน';
-            $divSiteQS = "SELECT * FROM `item_take_record` WHERE `taker` LIKE '" . $_POST['divName'] . "'"
+//echo '99 เลือกทั้งกลุ่มงานและสถานที่ใช้งาน';
+            $divSiteQS = "SELECT *"
+                    . " FROM `item_take_record`"
+                    . " WHERE `taker` LIKE '" . $_POST['divName'] . "'"
                     . " AND `site` LIKE '" . $_POST['siteName'] . "'";
             $tableHeader = array("รายการ", "ใช้ไป", "ใช้ที่", "เจ้าของ");
             $tableData = array("take_detail", "take_qty", "take_suffix", "site", "taker");
             $qryMsg = $_POST['divName'] . ", " . $_POST['siteName'];
+            $case = 3;
             break;
         }
-        //เลือกเฉพาะกลุ่มงาน
+//เลือกเฉพาะกลุ่มงาน
         if ($_POST['divName'] != "-- แยกตามกลุ่มงาน --") {
-            //echo '2-- แยกตามกลุ่มงาน --';
-            $divSiteQS = "SELECT `detail`,`quantity`,`suffix`,`owner` FROM `item` WHERE `owner` LIKE '" . $_POST['divName'] . "'";
+//echo '2-- แยกตามกลุ่มงาน --';
+            $divSiteQS = "SELECT `detail`,`quantity`,`suffix`,`owner`"
+                    . " FROM `item`"
+                    . " WHERE `owner`"
+                    . " LIKE '" . $_POST['divName'] . "'";
             $tableHeader = array("รายการ", "ใช้ไป", "เจ้าของ");
             $tableData = array("detail", "quantity", "suffix", "owner");
             $qryMsg = $_POST['divName'];
+            $case = 1;
             break;
         }
-        //เลือกเฉพาะสถานที่ใช้งาน
+//เลือกเฉพาะสถานที่ใช้งาน
         if ($_POST['siteName'] != "-- แยกตามสถานที่ใช้งาน --") {
-            //echo '3-- แยกตามสถานที่ใช้งาน --';
-            $divSiteQS = "SELECT * FROM `item_take_record` WHERE `site` LIKE '" . $_POST['siteName'] . "'";
-            //. " GROUP BY `take_detail`";
+//echo '3-- แยกตามสถานที่ใช้งาน --';
+            $divSiteQS = "SELECT take_detail, take_suffix, SUM(take_qty) as sum_takeQty, taker, site"
+                    . " FROM `item_take_record`"
+                    . " WHERE `site` LIKE '" . $_POST['siteName'] . "'"
+                    . " GROUP BY take_detail,site , taker";
+//. " GROUP BY `take_detail`";
             $tableHeader = array("รายการ", "ใช้ไป", "ใช้ที่", "เจ้าของ");
-            $tableData = array("take_detail", "take_qty", "take_suffix", "site", "taker");
+            $tableData = array("take_detail", "sum_takeQty", "take_suffix", "site", "taker");
             $qryMsg = $_POST['siteName'];
+            $case = 2;
             break;
         }
     } while (0);
 } else { //กดปุ่ม แสดงทั้งหมด
-    //echo "enter else";
-    $divSiteQS = "SELECT `detail`,`quantity`,`suffix`,`owner` FROM `item`";
+//echo "enter else";
+    $divSiteQS = "SELECT `detail`,`quantity`,`suffix`,`owner`"
+            . " FROM `item`";
     $tableHeader = array("รายการ", "คงเหลือ", "เจ้าของ");
     $tableData = array("detail", "quantity", "suffix", "owner");
     $qryMsg = "รายการสั่งซื้อ(ปกติ) ทั้งหมด";
     $_SESSION['lastDiv'] = "-- แยกตามกลุ่มงาน --";
     $_SESSION['lastSite'] = "-- แยกตามสถานที่ใช้งาน --";
+    $case = 0;
 }
 ?>
 
@@ -88,11 +103,11 @@ if (isset($_POST['submitBtn'])) {
         /* ไม่ใช้ case unauthen เพราะไม่มีสิทธิ์เข้าหน้านี้อยู่แล้ว */
         include 'navbar.php';
 
-        
-          echo 'SESSION = ';
-          print_r($_SESSION);
-          echo '<br/>POST = <br/>';
-          print_r($_POST); 
+
+        echo 'SESSION = ';
+        print_r($_SESSION);
+        echo '<br/>POST = <br/>';
+        print_r($_POST);
         ?>
 
         <div class="row">
@@ -172,50 +187,118 @@ if (isset($_POST['submitBtn'])) {
 
 
 
-                        <?php
-                        $divSiteQry = mysqli_query($connection, $divSiteQS);
-                        $divSiteCount = mysqli_num_rows($divSiteQry);
-                        if ($divSiteCount == 0) {
-                            ?>
-                            <div><b>คำค้น: </b><?= $qryMsg; ?> (0 รายการ)</div>
-                        <?php } else { ?>
-                            <div class="col-md-12">
-                                <div><b>คำค้น: </b><?= $qryMsg ?> (<?= $divSiteCount ?> รายการ)</div>
-                                <table class="table table-bordered table-condensed table-striped table-hover">
-                                    <thead>
-                                        <tr align="center">
-                                            <?php
-                                            foreach ($tableHeader as $value) {
-                                                echo "<th>" . $value . "</th>";
-                                            }
-                                            ?>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <?php
-                                        //echo '<br/>' . $divSiteQS;
-                                        while ($rowDivSite = mysqli_fetch_assoc($divSiteQry)) {
-                                            ?>
+
+
+
+
+
+                        <?php //ตรงนี้แยกเป็นเคส -1, 0, 1, 2, 3 
+                        //โดยเคส 2,3 ต้องเขียนแยก เพราะคิวรี่เอาสถานที่ใช้งาน ซึ่งต้องดึงจาก :take_record ไม่ใช่ :item
+                        if ($case == 2 || $case == 3) {
+                            $divSiteQry = mysqli_query($connection, $divSiteQS);
+                            $divSiteCount = mysqli_num_rows($divSiteQry);
+                            if ($divSiteCount == 0) { //ค้นแล้วเจอ 0 รายการ
+                                ?>
+                                <div><b>คำค้น: </b><?= $qryMsg; ?> (0 รายการ)</div>
+                            <?php } else { ?>
+                                <div class="col-md-12">
+                                    <div><b>คำค้น: </b><?= $qryMsg ?> (<?= $divSiteCount ?> รายการ)</div>
+                                    <table class="table table-bordered table-condensed table-striped table-hover">
+                                        <thead>
                                             <tr align="center">
-                                                <td align="left">
-                                                    <a href="show_item.php?detail=<?= $rowDivSite['detail'] ?>&owner=<?= $rowDivSite['owner'] ?>&suffix=<?= $rowDivSite['suffix'] ?>" target="_blank">
-                                                        <?= $rowDivSite[$tableData[0]] ?>
-                                                    </a>
-                                                </td>
-                                                <td nowrap><?= $rowDivSite[$tableData[1]].' '.$rowDivSite[$tableData[2]] ?></td>
-                                                <td><?= $rowDivSite[$tableData[3]] ?></td>
-                                                <?php if (isset($rowDivSite[$tableData[4]])) { ?>
-                                                    <td><?= $rowDivSite[$tableData[4]] ?></td>
-                                                <?php } ?>
-                                                <?php if (isset($rowDivSite[$tableData[5]])) { ?>
-                                                    <td><?= $rowDivSite[$tableData[5]] ?></td>
-                                                <?php } ?>
+                                                <?php
+                                                foreach ($tableHeader as $value) {
+                                                    echo "<th>" . $value . "</th>";
+                                                }
+                                                ?>
                                             </tr>
-                                        <?php } ?>
-                                    </tbody>
-                                </table>
-                            </div> <!-- /.col-md-12 -->
+                                        </thead>
+                                        <tbody>
+
+                                            <?php
+                                            while ($rowDivSite = mysqli_fetch_assoc($divSiteQry)) {
+                                                ?>
+                                                <tr align="center">
+                                                    <td align="left">
+                                                        <a href="show_item.php?detail=<?= $rowDivSite['take_detail'] ?>&owner=<?= $rowDivSite['taker'] ?>&suffix=<?= $rowDivSite['take_suffix'] ?>" target="_blank">
+                                                            <?= $rowDivSite[$tableData[0]] ?>
+                                                        </a>
+                                                    </td>
+                                                    <td nowrap><?= $rowDivSite[$tableData[1]] . ' ' . $rowDivSite[$tableData[2]] ?></td>
+                                                    <td><?= $rowDivSite[$tableData[3]] ?></td>
+                                                    <?php if (isset($rowDivSite[$tableData[4]])) { ?>
+                                                        <td><?= $rowDivSite[$tableData[4]] ?></td>
+                                                    <?php } ?>
+                                                    <?php if (isset($rowDivSite[$tableData[5]])) { ?>
+                                                        <td><?= $rowDivSite[$tableData[5]] ?></td>
+                                                    <?php } ?>
+                                                </tr>
+                                            <?php } ?>
+
+                                        </tbody>
+                                    </table>
+                                </div> <!-- /.col-md-12 -->
+                                <?php
+                            }
+                        } else { //$case == -1, 0, 1
+                            $divSiteQry = mysqli_query($connection, $divSiteQS);
+                            $divSiteCount = mysqli_num_rows($divSiteQry);
+                            if ($divSiteCount == 0) { //ค้นแล้วเจอ 0 รายการ
+                                ?>
+                                <div><b>คำค้น: </b><?= $qryMsg; ?> (0 รายการ)</div>
+                            <?php } else { ?>
+                                <div class="col-md-12">
+                                    <div><b>คำค้น: </b><?= $qryMsg ?> (<?= $divSiteCount ?> รายการ)</div>
+                                    <table class="table table-bordered table-condensed table-striped table-hover">
+                                        <thead>
+                                            <tr align="center">
+                                                <?php
+                                                foreach ($tableHeader as $value) {
+                                                    echo "<th>" . $value . "</th>";
+                                                }
+                                                ?>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+
+                                            <?php
+                                            while ($rowDivSite = mysqli_fetch_assoc($divSiteQry)) {
+                                                ?>
+                                                <tr align="center">
+                                                    <td align="left">
+                                                        <a href="show_item.php?detail=<?= $rowDivSite['detail'] ?>&owner=<?= $rowDivSite['owner'] ?>&suffix=<?= $rowDivSite['suffix'] ?>" target="_blank">
+                                                            <?= $rowDivSite[$tableData[0]] ?>
+                                                        </a>
+                                                    </td>
+                                                    <td nowrap><?= $rowDivSite[$tableData[1]] . ' ' . $rowDivSite[$tableData[2]] ?></td>
+                                                    <td><?= $rowDivSite[$tableData[3]] ?></td>
+                                                    <?php if (isset($rowDivSite[$tableData[4]])) { ?>
+                                                        <td><?= $rowDivSite[$tableData[4]] ?></td>
+                                                    <?php } ?>
+                                                    <?php if (isset($rowDivSite[$tableData[5]])) { ?>
+                                                        <td><?= $rowDivSite[$tableData[5]] ?></td>
+                                                    <?php } ?>
+                                                </tr>
+                                            <?php } ?>
+
+                                        </tbody>
+                                    </table>
+                                </div> <!-- /.col-md-12 -->
+                            <?php } ?>
                         <?php } ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
 
                     </div><!-- Main container -->
                 </div> <!-- /.col-md-10 -->
