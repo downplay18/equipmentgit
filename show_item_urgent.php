65f24e42bd7b9f4f9a243ec8adde2c2e2de31310
downplay18 <downplay18@Flatt-PC>
2016-10-24 14:19:07 +0700

    เสร็จจริงๆ

---
+++ show_item_urgent.php
@@ -0,0 +1,132 @@
+<?php
+//var_dump($_SESSION);
+session_start();
+error_reporting(0);
+require_once 'connection.php';
+include 'root_url.php';
+
+if ($_SESSION['user_id'] == "") {
+    header("Location: $root_url/index.php", true, 302);
+    exit();
+}
+
+if (isset($_GET['detail'])) {
+    $_SESSION['urg_detail'] = $_GET['detail'];
+    $_SESSION['urg_adder'] = $_GET['adder'];
+    $_SESSION['urg_suffix'] = $_GET['suffix'];
+}
+?>
+
+<html>
+    <head>
+
+        <title>ADMIN</title>
+        <!-- Bootstrap Core CSS -->
+        <?php include 'main_head.php'; ?>
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs/jqc-1.12.3/dt-1.10.12/datatables.min.css"/>
+    </head>
+
+    <body>
+        <?php
+        /* navbar */
+        /* ไม่ใช้ case unauthen เพราะไม่มีสิทธิ์เข้าหน้านี้อยู่แล้ว */
+        include 'navbar.php';
+
+        echo 'SESSION = ';
+        print_r($_SESSION);
+        echo '<br/>POST = <br/>';
+        print_r($_POST);
+        ?>
+
+        <div class="row">
+            <div class="col-md-12">
+                <!-- Main container -->
+                <div class="container-fluid">
+                    <div class="page-header">
+                        <h2><?= $_SESSION['urg_detail'] ?> <small></small></h2>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="container-fluid">
+                <div class="col-md-12">
+                    <?php
+                    $urgentQS = "SELECT * FROM `item_urgent_record` WHERE `urg_detail` LIKE '" . $_SESSION['urg_detail'] . "'"; //มันแค่เช็ค add ใช้แค่ owner ไม่ใช่ division
+                    $urgentSize = count($urgentHeader);
+                    $addTakeQry = mysqli_query($connection, $urgentQS) or die("addTakeQry failed: " . mysqli_error($connection));
+                    ?>
+                    <b>กำลังแสดง:</b> รายการ = <?= $_SESSION['urg_detail']?>
+                    <table id="example" class="table table-bordered table-hover table-condensed table-striped" width="100%" data-display-length='-1'>
+                        <thead>
+                            <tr align="center">
+                                <th>รายการ</th>
+                                <th>จน.</th>
+                                <th>วันที่ตามบิลฯ</th>
+                                <th>ผู้ลงบันทึก</th>
+                                <th>ใช้เพื่อ</th>
+                                <th>ใช้ที่</th>
+                                <th>สลิป</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            while ($rowAddTake = mysqli_fetch_assoc($addTakeQry)) {
+                                echo '<tr align="center">';
+                                echo '<td align="left" nowrap>' . $rowAddTake['urg_detail'] . '</td>';
+                                echo '<td>' . $rowAddTake['urg_qty'] . ' ' . $rowAddTake['urg_suffix'] . '</td>';
+                                echo '<td>' . preg_replace("/(\d+)\D+(\d+)\D+(\d+)/", "$3-$2-$1", $rowAddTake['urg_slipDate']) . '</td>';
+                                echo '<td>' . $rowAddTake['urg_adder'] . '</td>';
+                                echo '<td>' . $rowAddTake['urg_purpose'] . '</td>';
+                                echo '<td>' . $rowAddTake['urg_site'] . '</td>';
+                                if ($rowAddTake['urg_slip'] != "") {
+                                    echo '<td width="1%"><a href="' . $rowAddTake['urg_slip'] . '" target=\'_blank\' "><span class="label label-success"><span class="glyphicon glyphicon-file"></span></span></td>';
+                                } else {
+                                    echo '<td width="1%"></td>';
+                                }
+                                echo '</tr>';
+                            }
+                            ?>
+                        </tbody>
+                    </table>
+                </div> <!-- /.col-md-12 -->
+            </div> <!-- /.container-fluid -->
+        </div> <!-- /.col-md-10 -->
+    </div> <!-- /.row -->
+
+    <?php include 'main_script.php'; ?>
+    <script> /*PREVENT DOUBLE SUBMIT: ทำให้ปุ่ม submit กดได้ครั้งเดียว ป้องกับปัญหาเนต lag แล้ว user กดเบิ้ล มันจะทำให้ส่งค่า 2 เท่า */
+        $(document).ready(function () {
+            $("#singleSubmitForm").submit(function () {
+                $("#singleSubmitBtn").attr("disabled", true);
+                return true;
+            });
+        });
+    </script>
+
+    <script> /*
+     $(document).ready(function () {
+     $('#example').DataTable({
+     dom: 'Bfrtip',
+     buttons: [
+     'copy', 'csv', 'excel', 'pdf', 'print'
+     ]
+     });
+     }); */
+    </script>
+
+    <script>
+        $(document).ready(function () {
+            var table = $('#example').DataTable({
+                lengthChange: false,
+                buttons: ['copy', 'excel', 'pdf', 'colvis']
+            });
+
+            table.buttons().container()
+                    .appendTo('#example_wrapper .col-sm-6:eq(0)');
+        });
+    </script>
+
+
+</body>
+</html>
