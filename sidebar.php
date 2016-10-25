27517851cd27b32e3dd8a51be2e68fb0574c6389
downplay18 <downplay18@Flatt-PC>
2016-10-25 10:44:20 +0700

    แก้เป็น 4หัวข้อ เพิ่มurgent

--- sidebar.php
+++ sidebar.php
@@ -1,7 +1,7 @@
 <div class="list-group">
     <a href="<?= $root_url ?>/_login_check.php" class="list-group-item active" align="center"><span class="glyphicon glyphicon-home"></span> หน้าหลัก</a>
-    <a href="<?= $root_url ?>/show.php" class="list-group-item"><span class="glyphicon glyphicon-search"></span> สืบค้น(ซื้อปกติ)<span class="badge">999</span></a>
-    <a href="<?= $root_url ?>/show_urgent.php" class="list-group-item"><span class="glyphicon glyphicon-search"></span> สืบค้น(ซื้อเร่งด่วน)<span class="badge">999</span></a>
+    <a href="<?= $root_url ?>/show.php" class="list-group-item"><span class="glyphicon glyphicon-search"></span> สืบค้น(ซื้อปกติ)</a>
+    <a href="<?= $root_url ?>/show_urgent.php" class="list-group-item"><span class="glyphicon glyphicon-search"></span> สืบค้น(ซื้อเร่งด่วน)</a>
     <?php if ($_SESSION['status'] == "KEY") { ?>
         <a href="<?= $root_url ?>/add.php" class="list-group-item"><span class="glyphicon glyphicon-plus"></span> เพิ่มใบสั่งซื้อ(ปกติ)</a>
         <a href="<?= $root_url ?>/add_urgent.php" class="list-group-item"><span class="glyphicon glyphicon-plus"></span> เพิ่มใบสั่งซื้อ(เร่งด่วน)</a>
